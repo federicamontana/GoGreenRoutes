e5b2de0e012574a3805a9c6df5ddc76f5bfc3672
Federica Montana <federicamontana@gmail.com>
2022-09-12 11:03:54 +0200

    Modifiche di venerdi

--- notebook/Utility_Fede.py
+++ notebook/Utility_Fede.py
@@ -19,14 +19,22 @@ def aggregation(sent,df_s,aggr,labels):
     return aggr.append(num), labels.append(sent)
 
 
-def count_words(sent,df,result_dn):
+def find_words(sent,df,result_dn):
     dn= pd.Series(df.apply(lambda i: i.astype(str).str.contains(sent).any(), axis=0),name='bool').to_frame().reset_index()
     result_dn[sent] = dn.loc[dn['bool']==True].drop('bool', axis=1)
-    d = result_dn.to_dict(orient='list')
     # mask = pd.Series(df.apply(lambda i: i.astype(str).str.contains(sent).any(), axis=0), name='bool')
     # df2 = pd.DataFrame({sent:mask.index, 'bool':mask.values})
     # result_dn[sent]= df2.loc[df2['bool']==True].drop('bool', axis=1)
-    return d
+    return result_dn.to_dict(orient='list')
+
+def find_values(x,sentiment_list):
+    #https://stackoverflow.com/questions/64675132/pandas-find-all-words-from-row-in-dataframe-match-with-list
+    anticipation = []
+    for value in sentiment_list:
+        for word in x.split():
+            if word == value:
+                anticipation.append(word)
+    return ' '.join(anticipation)
 
 def new_df(sent,df,df2):
     df2[sent] = df.apply(lambda i: i.astype(str).str.contains(sent).any(), axis=0).astype(int)
