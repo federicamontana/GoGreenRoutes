d91c210c5ee595829330df1668548f1ea4378d0f
Federica Montana <federicamontana@gmail.com>
2022-09-14 16:43:35 +0200

    Modifica percorsi file

--- NRLC_Sentiment.py
+++ NRLC_Sentiment.py
@@ -11,7 +11,7 @@ from matplotlib import cm
 from Utility_Fede import aggregation_byparks,label,explode
 from Sentiment_lists import fear_list, anger_list, trust_list, surprise_list, positive_list, negative_list, sadness_list, disgust_list, joy_list, anticipation_list
 
-os.chdir(r'/Users/FEDERICA/Desktop/GoGreenRoutes/notebook')
+os.chdir(r'/Users/FEDERICA/Desktop/GoGreenRoutes')
 df = pd.read_csv('dataframe/df_completec.csv')
 
 #Vedo il sentimento in media nei parchi
